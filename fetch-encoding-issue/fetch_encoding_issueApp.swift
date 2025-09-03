f7b4b01d582dbc7e392a51b1181c32d9f2fe09a9
Simon Deutsch <simon.deutsch@upstream-mobility.at>
2025-09-03 17:37:49 +0200

    Initial Commit

---
+++ fetch-encoding-issue/fetch_encoding_issueApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetch_encoding_issueApp.swift
+//  fetch-encoding-issue
+//
+//  Created by Simon Deutsch on 03.09.25.
+//
+
+import SwiftUI
+
+@main
+struct fetch_encoding_issueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
